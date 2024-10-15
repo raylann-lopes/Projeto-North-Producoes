950c834662a035e222ae0e0abdaacf42216ecc73
raylann_lopes <raylanlopes@icloud.com>
2024-10-15 15:19:44 -0300

    git commit

--- JS/script.js
+++ JS/script.js
@@ -2,18 +2,18 @@ let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open');
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('open');
 };
 
-const sr = scrollReveal({
-    distance: '65px',
-    duration: 2600,
-    delay: 450,
-    reset: true
+const sr = ScrollReveal({
+  distance: '65px',
+  duration: 2600,
+  delay: 450,
+  reset: true
 });
 
-sr.reveal ('.north-text',{delay: 200, origin:'top'});
-sr.reveal ('.north-img',{delay: 450, origin:'top'});
-sr.reveal ('.icons',{delay: 500, origin:'left'});
-sr.reveal ('.scroll-down',{delay: 500, origin:'right'});
+sr.reveal('.north-text', { delay: 200, origin: 'top' });
+sr.reveal('.north-img', { delay: 450, origin: 'top' });
+sr.reveal('.icons', { delay: 500, origin: 'left' });
+sr.reveal('.scroll-down', { delay: 500, origin: 'right' });
