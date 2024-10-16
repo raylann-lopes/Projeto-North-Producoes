d02c35efb23c266236f86e7c2e146a8fb6b46418
raylann_lopes <raylanlopes@icloud.com>
2024-10-15 21:05:27 -0300

    git commit

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,5 @@
+/*Script Home Page*/
+
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
@@ -17,3 +19,15 @@ sr.reveal('.north-text', { delay: 200, origin: 'top' });
 sr.reveal('.north-img', { delay: 450, origin: 'top' });
 sr.reveal('.icons', { delay: 500, origin: 'left' });
 sr.reveal('.scroll-down', { delay: 500, origin: 'right' });
+
+
+/*Script About Page*/
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
