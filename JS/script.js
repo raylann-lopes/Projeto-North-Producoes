e7497fc390f153d34c5bd440a6296d1229db4058
raylann_lopes <raylanlopes@icloud.com>
2024-10-17 13:47:35 -0300

    git commit -m "Adicionando novas imagens e arquivos > source/Ana e Rian png.png "

--- JS/script.js
+++ JS/script.js
@@ -18,4 +18,40 @@ const sr = ScrollReveal({
 sr.reveal(".north-text", { delay: 200, origin: "top" });
 sr.reveal(".north-img", { delay: 450, origin: "top" });
 sr.reveal(".icons", { delay: 500, origin: "left" });
+sr.reveal(".image1", { delay: 300, origin: "right" });
 sr.reveal(".scroll-down", { delay: 500, origin: "right" });
+
+
+/*Script Cards*/
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerslides: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: "true",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakPoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+sr.reveal(".swiper-slide", { delay: 200, origin: "top" });
